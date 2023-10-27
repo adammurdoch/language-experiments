65ed58e3f4462bd2e25bfbc49eb50fdef2fda7d7
Adam Murdoch <adam@gradle.com>
2023-10-27 13:59:54 +1100

    Rust cli app updates.

--- rust/cli-app/src/main.rs
+++ rust/cli-app/src/main.rs
@@ -1,21 +1,27 @@
 #![allow(unused)]
 
 use clap::Parser;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
     println!("Searching for '{}' in {}", args.pattern, args.path.display());
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    let file = File::open(&args.path).expect("could not open file");
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let text = line.expect("could not read file");
+        if text.contains(&args.pattern) {
+            println!("{}", text);
         }
     }
 }
