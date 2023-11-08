f78d14b4b822447c0d4e2fe13406fe55364b2edb
Adam Murdoch <adam@gradle.com>
2023-11-08 15:12:16 +1100

    Refactor Go application.

--- go/cli-app/app.go
+++ go/cli-app/app.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 import "example/search"
 import "github.com/urfave/cli/v2"
@@ -17,7 +14,6 @@ func main() {
 			pattern := cCtx.Args().Get(0)
 			path := cCtx.Args().Get(1)
 			search.Search(pattern, path)
-			xsearch(pattern, path)
 			return nil
 		},
 	}
@@ -25,28 +21,4 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func xsearch(pattern string, path string) {
-	file, err := os.Open(path)
-	if err != nil {
-		fmt.Printf("Error opening the file: %v\n", err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-	lineNumber := 1
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, pattern) {
-			fmt.Printf("line %d: %s\n", lineNumber, line)
-		}
-		lineNumber++
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading the file: %v\n", err)
-	}
-
-	_ = file.Close()
 }
--- go/search/search.go
+++ go/search/search.go
@@ -1,7 +1,34 @@
 package search
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Search(pattern string, path string) {
 	fmt.Printf("Searching for '%s' in %s\n", pattern, path)
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Error opening the file: %v\n", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, pattern) {
+			fmt.Printf("line %d: %s\n", lineNumber, line)
+		}
+		lineNumber++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading the file: %v\n", err)
+	}
+
+	_ = file.Close()
 }
