c13b1a235c978ac2da8cb8194a552abb39dee1c0
Adam Murdoch <adam@gradle.com>
2023-11-10 11:11:40 +1100

    Changes to node.js app.

--- node-js/cli-app/index.js
+++ node-js/cli-app/index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const readline = require('readline');
 const program = require('commander');
 const search = require('search');
 
@@ -11,25 +9,6 @@ program
     .argument('<search-text>', 'The text to search for')
     .argument('<file>', 'The file to search')
     .action((searchText, file) => {
-        doSearch(searchText, file)
+        search.search(searchText, file)
     })
     .parse();
-
-function doSearch(searchText, file) {
-    search.search(searchText, file)
-
-    const fileStream = fs.createReadStream(file);
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });
-
-    let lineCounter = 0;
-
-    rl.on('line', (line) => {
-        lineCounter++;
-        if (line.includes(searchText)) {
-            console.log(`Line ${lineCounter}: ${line}`);
-        }
-    });
-}
--- node-js/search/index.js
+++ node-js/search/index.js
@@ -1,4 +1,22 @@
 
+const fs = require('fs');
+const readline = require('readline');
+
 exports.search = (searchText, file) => {
     console.log(`file=${file}, search=${searchText}`);
+
+    const fileStream = fs.createReadStream(file);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
+
+    let lineCounter = 0;
+
+    rl.on('line', (line) => {
+        lineCounter++;
+        if (line.includes(searchText)) {
+            console.log(`Line ${lineCounter}: ${line}`);
+        }
+    });
 }
