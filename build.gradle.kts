32c3f939c7ea88d27ca723cf8a3ca48ff30ac57c
Adam Murdoch <adam@gradle.com>
2023-11-24 17:50:12 +1100

    Tweak `run` task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,48 @@
-val rustCli by tasks.registering {
-    doLast {
-        exec {
-            commandLine = listOf("cargo", "run", "car", "../../build.gradle.kts")
-            workingDir = file("rust/cli-app")
+val apps = Apps(project) {
+    app("rust-cli", "rust/cli-app", listOf("cargo", "run"))
+    app("go-cli", "go/cli-app", listOf("go", "run", "."))
+    app("node-js-cli", "node-js/cli-app", listOf("node", "index.js"))
+    app("java-bazel-cli", "java/bazel", listOf("bazel-bin/cli-app"), listOf("bazel", "build", "cli-app"))
+}
+apps.registerTasks()
+
+class Apps(private val project: Project, builder: Apps.() -> Unit) {
+    val apps = mutableListOf<CliApp>()
+
+    init {
+        builder(this)
+    }
+
+    fun app(name: String, directory: String, runCommand: List<String>, buildCommand: List<String>? = null) {
+        apps.add(CliApp(name, project.file(directory), runCommand, buildCommand))
+    }
+
+    fun registerTasks() {
+        val runTasks = apps.map { app ->
+            project.tasks.register(app.name) {
+                doLast {
+                    if (app.buildCommand != null) {
+                        exec {
+                            commandLine = app.buildCommand
+                            workingDir = app.directory
+                        }
+                    }
+                    exec {
+                        commandLine = app.runCommand + listOf("car", "../../build.gradle.kts")
+                        workingDir = app.directory
+                    }
+                }
+            }
+        }
+        project.tasks.register("run") {
+            dependsOn(runTasks)
         }
     }
 }
 
-val goCli by tasks.registering {
-    doLast {
-        exec {
-            commandLine = listOf("go", "run", ".", "car", "../../build.gradle.kts")
-            workingDir = file("go/cli-app")
-        }
-    }
-}
-
-val nodeJsCli by tasks.registering {
-    doLast {
-        exec {
-            commandLine = listOf("node", "index.js", "car", "../../build.gradle.kts")
-            workingDir = file("node-js/cli-app")
-        }
-    }
-}
-
-val javaBazelCli by tasks.registering {
-    doLast {
-        exec {
-            commandLine = listOf("bazel", "build", "cli-app")
-            workingDir = file("java/bazel")
-        }
-        exec {
-            commandLine = listOf("bazel-bin/cli-app", "car", "../../build.gradle.kts")
-            workingDir = file("java/bazel")
-        }
-    }
-}
-
-tasks.register("run") {
-    dependsOn(rustCli, goCli, nodeJsCli, javaBazelCli)
-}
+class CliApp(
+    val name: String,
+    val directory: File,
+    val runCommand: List<String>,
+    val buildCommand: List<String>? = null
+)
